a8fc839c9782bbe090238a8d1d5f5056e8c55864
hkho484 <133361308+hkho484@users.noreply.github.com>
2023-06-09 13:54:41 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
 radio.onReceivedValue(function (name, value) {
     if (name == "posx") {
-    	
+        otro.change(LedSpriteProperty.X, value)
+    } else if (name == "posy") {
+        otro.change(LedSpriteProperty.Y, value)
     }
 })
+let otro: game.LedSprite = null
 basic.clearScreen()
 radio.setGroup(1)
 let yo = game.createSprite(2, 2)
-let otro = game.createSprite(4, 4)
+otro = game.createSprite(4, 4)
 let tiempo = 500
 basic.forever(function () {
 	
