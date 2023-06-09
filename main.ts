7e1f50b37f271aeb978db2d3a59a8d0d205e6e6c
hkho484 <133361308+hkho484@users.noreply.github.com>
2023-06-09 13:52:31 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "posx") {
+    	
+    }
+})
+basic.clearScreen()
+radio.setGroup(1)
+let yo = game.createSprite(2, 2)
+let otro = game.createSprite(4, 4)
+let tiempo = 500
+basic.forever(function () {
+	
+})
