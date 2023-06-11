51a6ac8fdbcbba6c6ffefb0dc750b02e4d44624b
hkho484 <133361308+hkho484@users.noreply.github.com>
2023-06-11 21:19:27 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,5 +12,17 @@ let yo = game.createSprite(2, 2)
 otro = game.createSprite(4, 4)
 let tiempo = 500
 basic.forever(function () {
-	
+    if (input.acceleration(Dimension.X) >= 500) {
+        yo.change(LedSpriteProperty.X, 1)
+        radio.sendValue("posx", yo.get(LedSpriteProperty.X))
+    } else if (input.acceleration(Dimension.X) < -500) {
+        yo.change(LedSpriteProperty.X, -1)
+        radio.sendValue("posx", yo.get(LedSpriteProperty.X))
+    } else if (input.acceleration(Dimension.Y) < -500) {
+        yo.change(LedSpriteProperty.Y, -1)
+        radio.sendValue("posy", yo.get(LedSpriteProperty.Y))
+    } else if (input.acceleration(Dimension.Y) >= 500) {
+        yo.change(LedSpriteProperty.Y, -1)
+        radio.sendValue("posy", yo.get(LedSpriteProperty.Y))
+    }
 })
